26e370d0b61db8ec9a278dafbbdc5c5805900864
nas- <nasil2nd@gmail.com>
2020-12-04 19:10:06 +0100

    Added day 4
    Signed-off-by: nas- <nasil2nd@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+Solutions to [adventofcode](https://adventofcode.com/)
+ 2020, implemented in python
--- day4/day4.py
+++ day4/day4.py
@@ -1,9 +1,9 @@
-import os
 import re
 
 fin = open("input.txt", "r")
 
 passports = [data.replace('\n', ' ').strip() for data in fin.read().split("\n\n")]
+
 
 class Passport(object):
     def __init__(self, data: str):
@@ -24,8 +24,9 @@ class Passport(object):
             byr_valid = 1920 <= int(self.features.get('byr')) <= 2002
             ihr_valid = 2010 <= int(self.features.get('iyr')) <= 2020
             eyr_valid = 2020 <= int(self.features.get('eyr')) <= 2030
-            hgt_valid = (self.features.get('hgt').endswith('cm') and 150 <= int(self.features.get('hgt')[:-2]) <= 193) \
-                        or (self.features.get('hgt').endswith('in') and 59 <= int(self.features.get('hgt')[:-2]) <= 76)
+            hgt_valid = (
+                self.features.get('hgt').endswith('cm') and 150 <= int(self.features.get('hgt')[:-2]) <= 193) \
+            or (self.features.get('hgt').endswith('in') and 59 <= int(self.features.get('hgt')[:-2]) <= 76)
             hcl_valid = bool(re.fullmatch(r'#[0-9a-f]{6}', self.features.get('hcl')))
             ecl_valid = self.features.get('ecl') in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
             pid_valid = bool(re.fullmatch(r'\d{9}', self.features.get('pid')))
