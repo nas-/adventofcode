8c0ee4783097cba33bd363a63e660a0d0272aa20
nas- <nas-@users.noreply.github.com>
2020-12-13 20:02:54 +0100

    Added day 13

---
+++ day13/day13.py
@@ -0,0 +1,37 @@
+import os
+import functools
+import copy
+
+fin = open("input.txt", "r")
+
+data = [data.strip() for data in fin.readlines()]
+earliest_time = int(data[0])
+busses = data[1].split(',')
+
+index = earliest_time
+unfound = True
+busID = 0
+while unfound:
+    index += 1
+    for bus in busses:
+        if bus == 'x':
+            continue
+        bus = int(bus)
+        if index % bus == 0:
+            unfound = False
+            busID = bus
+            break
+
+print(f'Part 1: {(index - earliest_time) * busID}')
+
+a = set(busses.copy())
+a.remove('x')
+setss = sorted(list(set(map(int, a))))
+
+jump = setss[0]
+index_2 = 0
+for element in setss[1:]:
+    while (index_2 + busses.index(str(element))) % element != 0:
+        index_2 += jump
+    jump *= element
+print(f'Part 2: {index_2}')
